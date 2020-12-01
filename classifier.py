75105374e9e0391e650944defd0fc47ee4a4e6f2
borkos97 <63650403+borkos97@users.noreply.github.com>
2020-12-01 15:04:05 +0100

    classifier

---
+++ classifier.py
@@ -0,0 +1,50 @@
+from skmultiflow.data import FileStream
+from skmultiflow.evaluation import EvaluatePrequential
+
+from skmultiflow.lazy import KNNClassifier
+from skmultiflow.trees import HoeffdingTreeClassifier
+from skmultiflow.meta import AdaptiveRandomForestClassifier
+from skmultiflow.rules import VeryFastDecisionRulesClassifier
+from skmultiflow.meta import AdditiveExpertEnsembleClassifier
+from skmultiflow.meta import AccuracyWeightedEnsembleClassifier
+
+PATH = './airlines.csv'
+
+
+def flow_detection_classifier(classifier, stream):
+    evaluator = EvaluatePrequential(show_plot=True, pretrain_size=2000, max_samples=50000)
+    evaluator.evaluate(stream=stream, model=classifier)
+    return evaluator
+
+
+def make_stream(path, classifier):
+    stream = FileStream(path)
+    evaluator = flow_detection_classifier(classifier, stream)
+    stream = evaluator.stream.y
+    return stream
+
+
+# Streams based on classifiers:
+# KNNClassifier
+make_stream(PATH, KNNClassifier(n_neighbors=5, max_window_size=1000, leaf_size=30))
+make_stream(PATH, KNNClassifier(n_neighbors=8, max_window_size=2000, leaf_size=40))
+#
+# # HoeffdingTreeClassifier
+make_stream(PATH, HoeffdingTreeClassifier(memory_estimate_period=1000000, grace_period=200, leaf_prediction='nba'))
+make_stream(PATH, HoeffdingTreeClassifier(memory_estimate_period=2000000, grace_period=300, leaf_prediction='mc'))
+#
+# # AdditiveExpertEnsembleClassifier
+make_stream(PATH, AdditiveExpertEnsembleClassifier(n_estimators=5, beta=0.8, gamma=0.1, pruning='weakest'))
+make_stream(PATH, AdditiveExpertEnsembleClassifier(n_estimators=8, beta=0.9, gamma=0.3, pruning='oldest'))
+#
+# # VeryFastDecisionRulesClassifier
+make_stream(PATH, VeryFastDecisionRulesClassifier(grace_period=200, tie_threshold=0.05, max_rules=20))
+make_stream(PATH, VeryFastDecisionRulesClassifier(grace_period=300, tie_threshold=0.1, max_rules=30))
+#
+# # AdaptiveRandomForestClassifier
+make_stream(PATH, AdaptiveRandomForestClassifier(n_estimators=10, lambda_value=6, performance_metric='acc'))
+make_stream(PATH, AdaptiveRandomForestClassifier(n_estimators=20, lambda_value=8, performance_metric='kappa'))
+
+# # AccuracyWeightedEnsembleClassifier
+make_stream(PATH, AccuracyWeightedEnsembleClassifier(n_estimators=10, n_kept_estimators=30, window_size=200, n_splits=5))
+make_stream(PATH, AccuracyWeightedEnsembleClassifier(n_estimators=15, n_kept_estimators=40, window_size=300, n_splits=8))
--- driftflow.py
+++ driftflow.py
@@ -1,115 +1,127 @@
 import pandas as pd
-from skmultiflow.data import DataStream, TemporalDataStream
-from skmultiflow.trees import HoeffdingTreeClassifier
-from skmultiflow.evaluation import EvaluatePrequential
+import matplotlib.pyplot as plt
+from skmultiflow.data import DataStream
 from skmultiflow.drift_detection.eddm import EDDM
 from skmultiflow.drift_detection.hddm_a import HDDM_A
 from skmultiflow.drift_detection import PageHinkley, DDM
 from skmultiflow.drift_detection.hddm_w import HDDM_W
 
 
+def plots(stream, detected_change, id, name, beginning_stream, end_stream, detection_size):
+    x = plt.plot(stream[beginning_stream:end_stream], c='g')
+    plt.scatter(id[:detection_size], detected_change[:detection_size], c='r', marker='o', s=15)
+    plt.setp(x, linewidth=0.5)
+    plt.title('Funkcja dryfu dla algorytmu %s' % (name))
+    plt.show()
+
+
 def prepare_data(filename):
-    data = pd.read_csv(filename, header=0, sep=',', parse_dates=['Date'])
+    data = pd.read_csv(filename, header=0, sep=',')
     nr_cols_float = list(data.select_dtypes(include='number').columns)
     nr_cols_string = list(data.select_dtypes(include='object').columns)
     data[nr_cols_float] = data[nr_cols_float].fillna(data[nr_cols_float].mean())
     data.dropna(axis='index', inplace=True)
-    data['Date'] = pd.to_datetime(data['Date']).sub(pd.Timestamp('2008-12-01')).dt.days
     data = pd.get_dummies(data, columns=nr_cols_string)
     return data
 
+
 def check_nulls(data):
     print(data.isnull().sum())
+
 
 def make_stream(data):
     stream = DataStream(data, y=None, target_idx=-1, n_targets=1, cat_features=None, name=None, allow_nan=False)
     stream = stream.y
     return stream
 
+
 def eddm(stream):
     detected_change = []
-    # detected_warning = []
+    id = []
     eddm = EDDM()
     data_stream = stream
     for i in range(len(stream)):
         eddm.add_element(data_stream[i])
-        # if eddm.detected_warning_zone():
-        #     detected_warning.append((data_stream[i]))
-        #     print("Warning zone has been detected in data: {}"
-        #           " - of index: {}".format(data_stream[i], i))
         if eddm.detected_change():
-            detected_change.append((data_stream[i]))
+            detected_change.append(data_stream[i])
+            id.append(i)
+
             print("Change has been detected in data: {}"
                   " - of index: {}".format(data_stream[i], i))
     print("EDDM Detected changes: " + str(len(detected_change)))
-    # print("EDDM Detected warning zones: " + str(len(detected_warning)))
+    plots(stream, detected_change, id, 'EDDM', 0, 500, 2)
 
 
 def hddm_a(stream):
     detected_change = []
-    detected_warning = []
+    id = []
     hddm_a = HDDM_A()
     data_stream = stream
     for i in range(len(stream)):
         hddm_a.add_element(data_stream[i])
-        # if hddm_a.detected_warning_zone():
-        #     detected_warning.append((data_stream[i]))
-        #     print('Warning zone has been detected in data: ' + str(data_stream[i]) + ' - of index: ' + str(i))
+
         if hddm_a.detected_change():
-            detected_change.append((data_stream[i]))
+            detected_change.append(data_stream[i])
+            id.append(i)
             print('Change has been detected in data: ' + str(data_stream[i]) + ' - of index: ' + str(i))
     print("HDDM_A Detected changes: " + str(len(detected_change)))
-    # print("HDDM_A Detected warning zones: " + str(len(detected_warning)))
+    plots(stream, detected_change, id, 'HDDM_A', 0, 3000, 0)
+
 
 def ph(stream):
     detected_change = []
+    id = []
     ph = PageHinkley()
     data_stream = stream
     for i in range(len(stream)):
         ph.add_element(data_stream[i])
         if ph.detected_change():
             print('Change has been detected in data: ' + str(data_stream[i]) + ' - of index: ' + str(i))
-            detected_change.append((data_stream[i]))
+            detected_change.append(data_stream[i])
+            id.append(i)
     print("PH Detected changes: " + str(len(detected_change)))
+    plots(stream, detected_change, id, 'PH', 10000, 20000, 1)
+
 
 def hddm_w(stream):
     detected_change = []
-    # detected_warning = []
+    id = []
     hddm_w = HDDM_W()
     data_stream = stream
     for i in range(len(stream)):
         hddm_w.add_element(data_stream[i])
-        # if hddm_w.detected_warning_zone():
-        #     print('Warning zone has been detected in data: ' + str(data_stream[i]) + ' - of index: ' + str(i))
-        #     detected_warning.append((data_stream[i]))
+
         if hddm_w.detected_change():
             print('Change has been detected in data: ' + str(data_stream[i]) + ' - of index: ' + str(i))
             detected_change.append((data_stream[i]))
+            id.append(i)
     print("HDDM_W Detected changes: " + str(len(detected_change)))
-    # print("HDDM_W Detected warning zones: " + str(len(detected_warning)))
+    plots(stream, detected_change, id, 'HDDM_W', 0, 3000, 3)
+
 
 def ddm(stream):
     detected_change = []
-    # detected_warning = []
+    id = []
     ddm = DDM()
     data_stream = stream
     for i in range(len(stream)):
         ddm.add_element(data_stream[i])
-        # if ddm.detected_warning_zone():
-        #     print('Warning zone has been detected in data: ' + str(data_stream[i]) + ' - of index: ' + str(i))
-        #     detected_warning.append((data_stream[i]))
+
         if ddm.detected_change():
             print('Change has been detected in data: ' + str(data_stream[i]) + ' - of index: ' + str(i))
-            detected_change.append((data_stream[i]))
+            detected_change.append(data_stream[i])
+            id.append(i)
     print("DDM Detected changes: " + str(len(detected_change)))
-    # print("DDM Detected warning zones: " + str(len(detected_warning)))
+    plots(stream, detected_change, id, 'DDM', 8000, 10000, 1)
+
 
 data = prepare_data('weatherAUS.csv')
-# check_nulls(data)
+check_nulls(data)
 stream = make_stream(data)
+
+#Drift detection:
 eddm(stream)
-# hddm_a(stream)
-# ph(stream)
-# hddm_w(stream)
-# ddm(stream)
-pass
+hddm_a(stream)
+ph(stream)
+hddm_w(stream)
+ddm(stream)
