2b8f29c2fd5500168892769911fa355de6240196
borkos97 <63650403+borkos97@users.noreply.github.com>
2020-12-07 00:48:45 +0100

    classifier

--- classifier.py
+++ classifier.py
@@ -28,23 +28,23 @@ def make_stream(path, classifier):
 # KNNClassifier
 make_stream(PATH, KNNClassifier(n_neighbors=5, max_window_size=1000, leaf_size=30))
 make_stream(PATH, KNNClassifier(n_neighbors=8, max_window_size=2000, leaf_size=40))
-
-# HoeffdingTreeClassifier
+#
+# # HoeffdingTreeClassifier
 make_stream(PATH, HoeffdingTreeClassifier(memory_estimate_period=1000000, grace_period=200, leaf_prediction='nba'))
 make_stream(PATH, HoeffdingTreeClassifier(memory_estimate_period=2000000, grace_period=300, leaf_prediction='mc'))
-
-# AdditiveExpertEnsembleClassifier
+#
+# # AdditiveExpertEnsembleClassifier
 make_stream(PATH, AdditiveExpertEnsembleClassifier(n_estimators=5, beta=0.8, gamma=0.1, pruning='weakest'))
 make_stream(PATH, AdditiveExpertEnsembleClassifier(n_estimators=8, beta=0.9, gamma=0.3, pruning='oldest'))
-
-# VeryFastDecisionRulesClassifier
+#
+# # VeryFastDecisionRulesClassifier
 make_stream(PATH, VeryFastDecisionRulesClassifier(grace_period=200, tie_threshold=0.05, max_rules=20))
 make_stream(PATH, VeryFastDecisionRulesClassifier(grace_period=300, tie_threshold=0.1, max_rules=30))
-
-# AdaptiveRandomForestClassifier
+#
+# # AdaptiveRandomForestClassifier
 make_stream(PATH, AdaptiveRandomForestClassifier(n_estimators=10, lambda_value=6, performance_metric='acc'))
 make_stream(PATH, AdaptiveRandomForestClassifier(n_estimators=20, lambda_value=8, performance_metric='kappa'))
 
-# AccuracyWeightedEnsembleClassifier
+# # AccuracyWeightedEnsembleClassifier
 make_stream(PATH, AccuracyWeightedEnsembleClassifier(n_estimators=10, n_kept_estimators=30, window_size=200, n_splits=5))
 make_stream(PATH, AccuracyWeightedEnsembleClassifier(n_estimators=15, n_kept_estimators=40, window_size=300, n_splits=8))
